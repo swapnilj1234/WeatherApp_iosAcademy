72518e2b32d51b7c2fea187efaa153a863eb0dfd
swapnil <swap81733@gmail.com>
2020-06-23 18:05:18 +0530

    create folder for tableview cell

---
+++ WeatherApp_iosAcademy/HourlyTableViewCell/HourlyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HourlyTableViewCell.swift
+//  WeatherApp_iosAcademy
+//
+//  Created by swapnil jadhav on 23/06/20.
+//  Copyright © 2020 t. All rights reserved.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
